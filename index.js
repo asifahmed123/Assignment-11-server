e9a9d99037f28bb057137432b2f2cb7cd320ff13
asifahmed123 <ahmedasif092@gmail.com>
2023-05-20 03:53:43 +0600

    patch method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,14 +39,41 @@ async function run() {
     })
 
     app.get('/my-toys', async (req, res) => {
-      const mail = req.query.sellerEmail;
-      console.log(mail);
+      const email = req.query.sellerEmail;
+      console.log(email);
+      const query = { sellerEmail: email }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
     })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await toyCollection.insertOne(user)
       res.send(result);
+    })
+
+    app.patch('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedToy = req.body;
+      console.log(updatedToy.description);
+      const updateDoc = {
+        $set: {
+          price: updatedToy.price,
+          quantity: updatedToy.quantity,
+          description: updatedToy.description
+        },
+      };
+
+      const result = await toyCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
