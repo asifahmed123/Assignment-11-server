54a2e526700693438eeba549228d5f7b3d06f4f9
asifahmed123 <ahmedasif092@gmail.com>
2023-05-19 16:50:49 +0600

    users data implemented

--- index.js
+++ index.js
@@ -28,31 +28,37 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-     const toyCollection = client.db('toyHavenDB').collection('toys');
+    const toyCollection = client.db('toyHavenDB').collection('toys');
 
-     app.get('/toys', async(req, res) => {
-          const toyCategory = req.query.category;
-          console.log(toyCategory);
-          const query = {category: toyCategory}
-          const result = await toyCollection.find(query).toArray();
-          res.send(result);
-     })
+    app.get('/toys', async (req, res) => {
+      const toyCategory = req.query.category;
+      console.log(toyCategory);
+      const query = { category: toyCategory }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await toyCollection.insertOne(user)
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-//     await client.close();
+    //     await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-     res.send('toy-haven server is running')
+  res.send('toy-haven server is running')
 })
 
 app.listen(port, () => {
-     console.log(`server is running on port: ${port}`);
+  console.log(`server is running on port: ${port}`);
 })
